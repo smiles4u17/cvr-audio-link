943c38e82e0df713f12a94035499590c5aec428a
smiles4u17 <116783641+smiles4u17@users.noreply.github.com>
2023-04-22 12:04:55 -0700

    Add files via upload

---
+++ reup.bat
@@ -0,0 +1,4 @@
+rem be sure you're on the `dev` branch!
+git remote set-url origin https://github.com/DomNomNomVR/cvr-audio-link
+git pull
+git remote set-url origin https://github.com/smiles4u17/cvr-audio-link
